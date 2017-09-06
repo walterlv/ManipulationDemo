5cc4907b1cfa4a9034852d3c5d857426c1dbd98f
walterlv <xblero@gmail.com>
2017-09-06 20:11:30 +0800

    加入 PIMC COM 组件调试

--- ManipulationDemo/MainWindow.xaml.cs
+++ ManipulationDemo/MainWindow.xaml.cs
@@ -27,6 +27,20 @@ namespace ManipulationDemo
             };
             _timer.Tick += OnTick;
             _timer.Start();
+
+            DebugPimc();
+        }
+
+        private void DebugPimc()
+        {
+            var tablets = new TouchTabletCollection();
+            for (int ix = 0; ix < tablets.Count; ++ix)
+            {
+                Console.WriteLine("Found tablet {0} named {1}", ix + 1, tablets[ix].Name);
+                Console.WriteLine("  Type = {0}, Multi-touch supported = {1}", tablets[ix].Kind, tablets[ix].IsMultiTouch);
+                Console.WriteLine("  Input rectangle  = {0}", tablets[ix].InputRectangle);
+                Console.WriteLine("  Screen rectangle = {0}", tablets[ix].ScreenRectangle);
+            }
         }
 
         private Storyboard StylusDownStoryboard => (Storyboard) IndicatorPanel.FindResource("Storyboard.StylusDown");
