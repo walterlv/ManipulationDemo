ceb985d15818c39aa459ca9e35731e3431c021ae
walterlv <xblero@gmail.com>
2018-08-05 19:15:10 +0800

    创建启动快捷方式

--- ManipulationDemo/Program.cs
+++ ManipulationDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 namespace ManipulationDemo
 {
@@ -9,6 +10,10 @@ namespace ManipulationDemo
         [STAThread]
         static void Main(string[] args)
         {
+            // 创建启动快捷方式。
+            CreateStartupShorcut();
+
+            // 启动前一个进程实例。
             try
             {
                 var hwnd = FindWindow(null, "触摸以监视");
@@ -18,9 +23,45 @@ namespace ManipulationDemo
             {
             }
 
+            // 启动自己。
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        /// <summary>
+        /// 程序启动时在启动文件夹创建快捷方式。
+        /// </summary>
+        private static void CreateStartupShorcut()
+        {
+            // 定位启动快捷方式的路径。
+            var startupPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Startup),
+                "ManipulationDemo.lnk");
+
+            // 防止本程序的运行路径改变，所以每次启动都重写快捷方式。
+            if (File.Exists(startupPath))
+            {
+                File.Delete(startupPath);
+            }
+
+            // 创建快捷方式。
+            CreateShortcut(startupPath, "--startup");
+        }
+
+        /// <summary>
+        /// 为本程序创建一个快捷方式。
+        /// </summary>
+        /// <param name="lnkFilePath">快捷方式的完全限定路径。</param>
+        /// <param name="args">快捷方式启动程序时需要使用的参数。</param>
+        private static void CreateShortcut(string lnkFilePath, string args)
+        {
+            var shellType = Type.GetTypeFromProgID("WScript.Shell");
+            dynamic shell = Activator.CreateInstance(shellType);
+            var shortcut = shell.CreateShortcut(lnkFilePath);
+            shortcut.TargetPath = $@"""{Assembly.GetEntryAssembly().Location} {args}""";
+            shortcut.WorkingDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            shortcut.Save();
         }
 
         [DllImport("user32.dll", CharSet = CharSet.Unicode)]
